c2f285ef12cfb1d17680bedf2fdf9673b17200f8
JaehongDev <println@kakao.com>
2023-12-22 00:50:10 +0900

    Initial commit

---
+++ src/main/java/com/example/springbatchjenkinspractice/SpringBatchJenkinsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchjenkinspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchJenkinsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchJenkinsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbatchjenkinspractice/SpringBatchJenkinsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchjenkinspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchJenkinsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
